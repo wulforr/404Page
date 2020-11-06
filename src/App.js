9bb1571509ab4dbba9e24f975a1b4210030475b4
wulforr <gsshaurya@gmail.com>
2020-11-06 19:13:52 +0530

    added basic layout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import './App.css';
+import Scarecrow from './assets/Scarecrow.png'
 
 function App() {
   return (
     <div className="App">
       <header>
         <nav>
-          404 not found
+          {"404 not found"}
         </nav>
       </header>
       <main>
-        <div>
-          <img src="" />
+        <div className="scarecrow-wrapper">
+          <img src={Scarecrow} alt="Scarecrow" />
         </div>
-        <div>
+        <div className="content">
+          <div>
+          <h1>I have bad news for you</h1>
+          <p>
+          The page you are looking for might be removed or is temporarily unavailable
+          </p>
+          <button>Back to homepage</button>
+          </div>
         </div>
       </main>
     </div>
