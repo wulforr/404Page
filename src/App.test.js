e26693044776c516f89f80ab0a8ccd59307e6cae
wulforr <gsshaurya@gmail.com>
2020-11-08 01:59:50 +0530

    improved layout and wrote tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,34 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders 404 in navbar', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/404 NOT FOUND/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+test('renders bad news heading', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/I have bad news for you/i);
+  expect(linkElement).toBeInTheDocument();
+})
+
+test('renders temporarily unavailable paragraph', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/The page you are looking for might be removed or is temporarily unavailable/i);
+  expect(linkElement).toBeInTheDocument();
+})
+
+test('renders back to homepage button', () => {
+  render(<App />);
+  const linkElement = screen.getByRole("button");
+  expect(linkElement).toBeInTheDocument();
+})
+
+test('renders scarecrow image', () => {
+  render(<App />);
+  const linkElement = screen.getByAltText(/Scarecrow/i);
+  expect(linkElement).toBeInTheDocument();
+})
+
